acf926942bd909f8073254266359fccfaa632ce5
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-06 16:23:46 -0300

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //obrigatório se chamar baseURL para axios entender.
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://backendwithnode.herokuapp.com/'
 });
 
 export default api;
